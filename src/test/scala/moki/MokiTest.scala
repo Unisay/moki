245e562e8d0c6bc48cdfd049590d779164198064
Yuriy Lazarev <Yuriy.Lazarev@gmail.com>
2017-02-05 17:09:07 +0100

    Remove extra braces

--- src/test/scala/moki/MokiTest.scala
+++ src/test/scala/moki/MokiTest.scala
@@ -20,19 +20,17 @@ class MokiTest extends FlatSpec with MustMatchers {
       result[Task[Assertion]]
 
     runSync(services) {
-      http1 => http2 => http3 => {
-        for {
-          _         <- http1.setResponder(_ => Response(Status.Ok))
-          response  <- SimpleHttp1Client().expect[String](http1.uri)
-          paths1    <- http1.requests.take(1).map(_.uri.path).runLog.timed(10.second)
-          received2 <- http2.received
-          received3 <- http3.received
-        } yield {
-          response mustEqual ""
-          paths1 must contain theSameElementsAs Some("/")
-          received2 mustEqual 0
-          received3 mustEqual 0
-        }
+      http1 => http2 => http3 => for {
+        _         <- http1.setResponder(_ => Response(Status.Ok))
+        response  <- SimpleHttp1Client().expect[String](http1.uri)
+        paths1    <- http1.requests.take(1).map(_.uri.path).runLog.timed(10.second)
+        received2 <- http2.received
+        received3 <- http3.received
+      } yield {
+        response mustEqual ""
+        paths1 must contain theSameElementsAs Some("/")
+        received2 mustEqual 0
+        received3 mustEqual 0
       }
     }
   }
