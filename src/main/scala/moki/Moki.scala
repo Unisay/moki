0944161b4a91d2135556bf7922618086830b5385
Yuriy Lazarev <Yuriy.Lazarev@gmail.com>
2017-02-05 13:51:38 +0100

    MokiClient.setResponder

--- src/main/scala/moki/Moki.scala
+++ src/main/scala/moki/Moki.scala
@@ -2,40 +2,47 @@ package moki
 
 import fs2._
 import fs2.async._
-import fs2.async.immutable.Signal
-import fs2.async.mutable.Queue
+import fs2.async.mutable.{Queue, Signal}
 import fs2.interop.scalaz._
-import org.http4s._
+import org.http4s.{HttpService, _}
 import org.http4s.server.Server
 import org.http4s.server.blaze._
 
 import scalaz.concurrent.Task
-import scalaz.syntax.functor._
 
 object Moki {
 
-  def testService(host: String, port: Int): TestService[MokiClient, MokiClient] =
+  def httpService(host: String = "localhost", port: Int): TestService[MokiClient, MokiClient] =
     TestService(startServer(host, port), _.shutdownServer)
 
   def startServer(host: String, port: Int): Task[MokiClient] =
     for {
       queue  <- boundedQueue[Task, Request](maxSize = Int.MaxValue)
-      server <- server(queue, host, port)
-    } yield new MokiClient(queue, server)
+      signal <- Signal((_: Request) => Response(Status.NotFound))
+      server <- buildServer(queue, signal, host, port)
+    } yield new MokiClient(queue, signal, server)
 
-  private def server(queue: Queue[Task, Request], host: String, port: Int): Task[Server] = {
-    val httpService = HttpService {
+  private def buildServer(queue: Queue[Task, Request],
+                          signal: Signal[Task, Request => Response],
+                          host: String, port: Int): Task[Server] = {
+    val service = HttpService {
       case request =>
-        queue.enqueue1(request).as(Response(Status.Ok)) // TODO: consider offer?
+        for {
+          responder <- signal.get
+          _ <- queue.enqueue1(request)
+        } yield responder(request)
     }
     BlazeBuilder
       .bindHttp(port, host)
-      .mountService(httpService, "/")
+      .mountService(service, "/")
       .start
   }
 }
 
-class MokiClient private[moki](val queue: Queue[Task, Request], private val server: Server) {
+class MokiClient private[moki](private val queue: Queue[Task, Request],
+                               private val signal: Signal[Task, Request => Response],
+                               private val server: Server) {
+  def setResponder(f: Request => Response): Task[Unit] = signal set f
   def received: Task[Int] = queue.available.get.map(Int.MaxValue - _)
   def requests: Stream[Task, Request] = queue.dequeue
   def shutdownServer: Task[Unit] = server.shutdown
--- src/test/scala/moki/MokiTest.scala
+++ src/test/scala/moki/MokiTest.scala
@@ -3,6 +3,8 @@ package moki
 import fs2.interop.scalaz._
 import moki.TestService._
 import org.http4s.client.blaze.SimpleHttp1Client
+import org.http4s.dsl._
+import org.http4s.{Response, Status}
 import org.scalatest.{Assertion, FlatSpec, MustMatchers}
 
 import scala.concurrent.duration._
@@ -10,25 +12,29 @@ import scalaz.concurrent.Task
 
 class MokiTest extends FlatSpec with MustMatchers {
 
-  private val services =
-    Moki.testService("localhost", 8080) :>:
-    Moki.testService("localhost", 8081) :>:
-    Moki.testService("localhost", 8082) :>:
-    result[Task[Assertion]]
+  "Moki server" must "work as expected" in {
+    val services =
+      Moki.httpService(port = 8080) :>:
+      Moki.httpService(port = 8081) :>:
+      Moki.httpService(port = 8082) :>:
+      result[Task[Assertion]]
 
-  "Moki server" must "notify about incoming request" in runSync(services) {
-    mokiClient1 => mokiClient2 => mokiClient3 =>
+    runSync(services) {
+      http1 => http2 => http3 => {
         for {
+          _         <- http1.setResponder(_ => Response(Status.Ok))
           response  <- SimpleHttp1Client().expect[String]("http://localhost:8080")
-          paths1    <- mokiClient1.requests.take(1).map(_.uri.path).runLog.timed(10.second)
-          received2 <- mokiClient2.received
-          received3 <- mokiClient3.received
+          paths1    <- http1.requests.take(1).map(_.uri.path).runLog.timed(10.second)
+          received2 <- http2.received
+          received3 <- http3.received
         } yield {
           response mustEqual ""
           paths1 must contain theSameElementsAs Some("/")
           received2 mustEqual 0
           received3 mustEqual 0
         }
+      }
     }
+  }
 
 }
